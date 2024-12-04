8d2711043ad6267f4f223cb0ba1717302a6d9419
fahmy-dev <faahmy@proton.me>
2024-12-04 14:51:56 +0300

    final commit (2)

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 // the array with strings 
-const stringArray = ["1", "2", "3"];
+// const stringArray = ["1", "2", "3"];
 
 // to create a function which will create a copy of this array, and the convert it 
 function convertArray(strings) {
-    return parseInt(strings, 10);
+    return strings.map(str => {
+        if (parseFloat(str) === parseInt(str, 10)) {
+            return parseInt(str, 10);
+            } else {
+            return parseFloat(str);
+            }
+    });
 }
 
-const numArray = stringArray.map(convertArray); 
-
-console.log(numArray);
-
+console.log(convertArray(["1.5", "2.8", "3.0"]));
+console.log(convertArray(["0", "-10", "3.14"]));
 
 
+
+
+//function convertStringsToNumbers(arr) {
+    //if(!arr.every((str) => !isNaN(parseFloat(str)))) {
+       // return undefined;
+  //  }
+
+//return arr.map((str) => parseFloat(str));
+//}
+
+//console.log(convertStringsToNumbers(["1", "2", "3"]));
