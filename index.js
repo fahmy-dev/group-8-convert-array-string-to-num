28849108a37af5b4ed6c5f9c88d866d7b8b4e1dc
fahmy-dev <faahmy@proton.me>
2024-12-04 12:13:26 +0300

    final commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // the array with strings 
 const stringArray = ["1", "2", "3"];
+
+// to create a function which will create a copy of this array, and the convert it 
+function convertArray(strings) {
+    return parseInt(strings, 10);
+}
+
+const numArray = stringArray.map(convertArray); 
+
+console.log(numArray);
+
+
+
